36ddbd3044325358b1c497eafb4111d8c420d917
raj gautam <rajpunkeshwarrajgautam@gmail.com>
2026-08-19 12:07:15 +0530

    Load reference shell only on AIRA home

--- perplexity-clone/my-turborepo/apps/web/app/page.tsx
+++ perplexity-clone/my-turborepo/apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 
+import "./aira-reference.css";
 import { AiraPreloader } from "../components/AiraPreloader";
 import { SearchLayout } from "../components/SearchLayout";
 
@@ -9,7 +10,7 @@ function HomeSkeleton() {
 			<div className="flex min-h-dvh">
 				<div className="hidden w-[260px] border-r border-white/[0.07] bg-[#0d0d0d] md:block" />
 				<div className="flex-1 px-4 py-10">
-					<div className="mx-auto max-w-[780px] space-y-5 pt-24">
+					<div className="mx-auto max-w-[768px] space-y-5 pt-24">
 						<div className="mx-auto h-8 w-44 animate-pulse rounded bg-[#161616]" />
 						<div className="h-28 animate-pulse rounded-xl border border-white/[0.07] bg-[#161616]" />
 					</div>
